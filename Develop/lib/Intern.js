409607b8ca19f4c5c411779db3795aef8eca601a
Tiffany Crockett <crockett.tiff@gmail.com>
2021-02-02 11:21:53 -0700

    add lib files to new repo

--- Develop/lib/Intern.js
+++ Develop/lib/Intern.js
@@ -1,1 +1,21 @@
 // TODO: Write code to define and export the Intern class.  HINT: This class should inherit from Employee.
+const Employee = require ('./lib/Employee');
+
+    class Intern extends Employee { 
+    // constructor for specific class
+    constructor(name, id, email, school) { 
+        // super -  what is inherited from Employee
+        super(name, id, email)
+        // this.newThing = newThing/s in constructor
+        this.school = school;
+    } 
+    getRole() {
+        return 'Intern';
+    } 
+
+    getSchool() {
+        return this.school;
+    }
+} 
+
+module.exports = Intern
--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,1 +1,21 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
+const Employee = require ('./lib/Employee'); 
+
+class Manager extends Employee { 
+    // constructor for specific class
+    constructor(name, id, email, officeNumber) { 
+        // super -  what is inherited from Employee
+        super(name, id, email)
+        // this.newThing = newThing/s in constructor
+        this.officeNumber = officeNumber;
+    } 
+    getRole() {
+        return 'Manager';
+    } 
+
+    getOfficeNumber() {
+        return this.officeNumber;
+    }
+} 
+
+module.exports = Manager
